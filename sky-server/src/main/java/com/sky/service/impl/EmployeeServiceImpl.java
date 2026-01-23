bd442f60ff9e4b0a26bf355a85c3f272689e806d
SCUklr <89853493+SCUklr@users.noreply.github.com>
2026-01-23 21:13:47 +0800

    分类模块功能开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
@@ -49,8 +50,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        //对前端传来的密码进行md5加密
-        // password = DigestUtils.md5DigestAsHex(password.getBytes());
+        // TODO 后期需要进行md5加密，然后再进行比对
         password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
@@ -66,56 +66,46 @@ public class EmployeeServiceImpl implements EmployeeService {
         return employee;
     }
 
-    @Override
+    /**
+     * 新增员工
+     *
+     * @param employeeDTO
+     */
     public void save(EmployeeDTO employeeDTO) {
-        // DTO封装前端 需要 转换为Employee实体
         Employee employee = new Employee();
 
-        // employee.setName(employeeDTO.getName());
-        // 对象属性拷贝 属性名必须一致
+        //对象属性拷贝
         BeanUtils.copyProperties(employeeDTO, employee);
-        // 设置账号状态， 默认正常状态 1·正常 0·锁定 定义常量类而非硬编码
+
+        //设置账号的状态，默认正常状态 1表示正常 0表示锁定
         employee.setStatus(StatusConstant.ENABLE);
 
-        // 设置密码，默认123456 md5加密
-        employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
+        //设置密码，默认密码123456
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
 
-        // 设置当前记录创建、修改时间
+        //设置当前记录的创建时间和修改时间
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
 
-        // 设置创建和修改人的id
-        // TODO 后期需要动态获取
-        employee.setCreateUser(BaseContext.getCurrentId()); //目前写个假数据，后期修改
+        //设置当前记录创建人id和修改人id
+        employee.setCreateUser(BaseContext.getCurrentId());//目前写个假数据，后期修改
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
     }
 
     /**
-     * 启用禁用员工账号
+     * 分页查询
      *
-     * @param status
-     * @param id
+     * @param employeePageQueryDTO
+     * @return
      */
-    @Override
-    public void startOrStop(Integer status, Long id) {
-        // 只更新部分字段，使用Builder更直观（等价于new + set）
-        Employee employee = Employee.builder()
-                .status(status)
-                .id(id)
-                .build();
-        employeeMapper.update(employee); // 把更新甩给Mapper层让它来和数据库交互
-    }
-
-    @Override
     public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO) {
-        // 在EmployeeServiceImpl中实现pageQuery方法：
-        // select * from employee limit 0, 10
-        // 开始分页查询
+        // select * from employee limit 0,10
+        //开始分页查询
         PageHelper.startPage(employeePageQueryDTO.getPage(), employeePageQueryDTO.getPageSize());
 
-        Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);//后续定义
+        Page<Employee> page = employeeMapper.pageQuery(employeePageQueryDTO);
 
         long total = page.getTotal();
         List<Employee> records = page.getResult();
@@ -124,23 +114,37 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
 
     /**
+     * 启用禁用员工账号
+     *
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+
+        employeeMapper.update(employee);
+    }
+
+    /**
      * 根据id查询员工
      *
      * @param id
      * @return
      */
-    @Override
     public Employee getById(Long id) {
         Employee employee = employeeMapper.getById(id);
         employee.setPassword("****");
         return employee;
     }
+
     /**
      * 编辑员工信息
      *
      * @param employeeDTO
      */
-    @Override
     public void update(EmployeeDTO employeeDTO) {
         Employee employee = new Employee();
         BeanUtils.copyProperties(employeeDTO, employee);
