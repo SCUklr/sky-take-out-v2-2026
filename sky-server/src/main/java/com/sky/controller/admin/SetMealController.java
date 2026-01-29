c37740a5297c89d3e7c32a99c2886aeca3422100
SCUklr <89853493+SCUklr@users.noreply.github.com>
2026-01-29 17:31:52 +0800

    套餐setmeal模块功能开发（新增、分页、删除和修改）

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -0,0 +1,91 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetMealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * 套餐管理
+ */
+
+@RestController
+@RequestMapping("/admin/setmeal")
+@Api(tags = "套餐相关接口")
+@Slf4j // 自动为当前类生成一个日志对象 log，类型是 org.slf4j.Logger，你可以直接使用 log.info()、log.debug()、log.error() 等方法打日志，而不需要自己写那一行又长又烦的声明。
+public class SetMealController {
+    @Autowired
+    private SetMealService setMealService;
+
+    /**
+     * 新增套餐
+     * POST
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO) {
+        setMealService.saveWithDish(setmealDTO);
+        return Result.success();
+    }
+    /**
+     * 分页查询套餐
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("分页查询")
+    public Result<PageResult> page(SetmealPageQueryDTO setmealPageQueryDTO) {
+        PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 删除套餐
+     * @param ids
+     * @return
+     * */
+    @DeleteMapping
+    @ApiOperation("删除套餐")
+    public Result delete(@RequestParam List<Long> ids) {
+        setMealService.deleteBatch(ids);
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐，用于修改页面回显数据
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    // @PathVariable 告诉 Spring 把 URL 路径里的 {id} 绑定到这个参数 id 上。
+    public Result<SetmealVO> getById(@PathVariable Long id) {
+        SetmealVO setmealVO = setMealService.getByIdWithDish(id);
+        return Result.success(setmealVO);
+    }
+
+    /**
+     * 修改套餐
+     *
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("修改套餐")
+    public Result update(@RequestBody SetmealDTO setmealDTO) {
+        setMealService.update(setmealDTO);
+        return Result.success();
+    }
+}
