b13a1f95dbe3e26f2ceac15e10d6184cd973c754
SCUklr <89853493+SCUklr@users.noreply.github.com>
2026-02-04 23:44:24 +0800

    Day11:营业额数据统计代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -0,0 +1,46 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+
+/**
+ * 报表
+ */
+@RestController
+@RequestMapping("/admin/report")
+@Slf4j
+@Api(tags = "统计报表相关接口")
+public class ReportController {
+    @Autowired
+    private ReportService reportService;
+    /**
+     * 统计营业额数据
+     * GET
+     * /turnoverStatistics
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/turnoverStatistics")
+    @ApiOperation("营业额数据统计")
+    public Result<TurnoverReportVO> turnoverStatistics(
+            // 修复日期解析：MM 才是月份，mm 是分钟，导致 begin/end 解析失败
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                LocalDate begin,
+            // 修复日期解析：MM 才是月份，mm 是分钟，导致 begin/end 解析失败
+            @DateTimeFormat(pattern = "yyyy-MM-dd")
+                LocalDate end) {
+        return Result.success(reportService.getTurnover(begin, end));
+    }
+}
